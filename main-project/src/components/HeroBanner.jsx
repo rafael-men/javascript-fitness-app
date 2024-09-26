93ee0dabd95f97c3d41f9bbeea03a76d9b3b99ee
rafael-men <rafaelgoncalves1822@gmail.com>
2024-09-26 20:50:41 -0300

    hero section

---
+++ main-project/src/components/HeroBanner.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Box,Button,Stack,Typography} from '@mui/material'
+
+import Banner from '../assets/Runnin.jpg'
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{
+      mt: {lg: '212px',xs:'70px'},
+      ml: {sm: '50px'}
+    }} position='relative' p='20px'>
+      <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+        Fitness Club
+      </Typography>
+      <Typography fontWeight='700' sx={{
+        fontSize:{lg:'44px',xs:'40px'}
+      }} mb='23px' mt='30px'>
+        Need to get healthy?
+      </Typography>
+      <Typography fontSize='22px' lineHeight='35px' mb={5}>
+        Check out what we are offering:
+      </Typography>
+      <Button variant='container' color='error' href='#exercises' sx={{
+        backgroundColor:'#FF2625',
+        padding:'10px',
+        color:'white'
+      }}>Explore Exercises</Button>
+      <Typography fontWeight={600} sx={{
+        opacity:0.1,
+        display: {lg: 'block',xs: 'none'}
+      }} fontSize='200px'>
+        Health
+      </Typography>
+      <img src={Banner} className='hero-banner-img'/>
+    </Box>
+  )
+}
+
+export default HeroBanner
--- main-project/src/components/Home.jsx
+++ main-project/src/components/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from './HeroBanner'
+import Exercises from './Exercises'
+import ExerciseDetail from './ExerciseDetail'
+import SearchExercises from './SearchExercises'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
 
