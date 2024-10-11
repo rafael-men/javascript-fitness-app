078c125e08935120cc3398343c708eb445b34c4d
rafael-men <rafaelgoncalves1822@gmail.com>
2024-10-10 21:04:46 -0300

    scrollbar-error

--- main-project/src/components/BodyPart.jsx
+++ main-project/src/components/BodyPart.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import BodyPart from './BodyPart'
 
-const BodyPart = () => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+
+
   return (
-    <div>BodyPart</div>
+    <div style={{ display: 'flex', overflowX: 'auto' }}>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          id={item.id || item} // Alterado de itemId para id
+          title={item.id || item}
+          m='0 40px'
+          sx={{ cursor: 'pointer' }} // Adiciona um estilo de cursor
+        >
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </Box>
+      ))}
+    </div>
   )
 }
 
-export default BodyPart
+export default HorizontalScrollbar;
